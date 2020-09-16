ee87ce5cfb9bb61c0685fade5d90ef1b11b71073
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-16 14:25:23 +0100

    added Wins model

--- tennis/models.py
+++ tennis/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Wins(models.Model):
     name = models.CharField(max_length=100)
-    wins = models.PositiveIntegerField()
+    # wins = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
