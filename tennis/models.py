50641c8c4ec31bb3de8de98be304fe138aaf3604
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-16 13:05:02 +0100

    Added models but error occurs

--- tennis/models.py
+++ tennis/models.py
@@ -2,11 +2,18 @@
 from django.db import models
 
 
-# class Players(models.Model):
-#     name = models.CharField(max_length=100)
-#     age = models.PositiveIntegerField()
+class Wins(models.Model):
+    name = models.CharField(max_length=100)
+    wins = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
 
 
-# class Nationality(models.Model):
-#     name = models.CharField(max_length=50)
-#     Players = models.ForeignKey(Players, on_delete=models.SET_NULL)
+class Players(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    wins = models.ForeignKey(Wins, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
