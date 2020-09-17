b3f6ffc0258d649d75a9e42df5313b51442744e3
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-09-17 12:54:00 +0100

    added 404 error handler

--- lap4learn/urls.py
+++ lap4learn/urls.py
@@ -22,8 +22,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('signup/', user_views.signup, name='signup'),
-    path('tennis/', include('tennis.urls')),
+    path('', include('tennis.urls')),
     path('admin/', admin.site.urls),
 ]
 
+handler404 = 'tennis.views.not_found_404'
 handler500 = 'tennis.views.server_error'
