94fb50d05f16b79cf85a4b2832fb43c94b170f68
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-09-17 12:52:03 +0100

    Added models to form to create new player form

---
+++ tennis/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Players
+
+class NewPlayerForm(forms.ModelForm):
+    class Meta:
+        model = Players
+        fields = ['name', 'age', 'wins']
+        
+
+
