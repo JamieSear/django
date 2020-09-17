3028189c9fc64c12f4a2872c588e7e25962da54c
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-09-17 17:33:02 +0100

    added amount field to new player form

--- tennis/forms.py
+++ tennis/forms.py
@@ -4,7 +4,7 @@ from .models import Players
 class NewPlayerForm(forms.ModelForm):
     class Meta:
         model = Players
-        fields = ['name', 'age', 'wins']
+        fields = ['name', 'age', 'wins', 'amount']
         
 
 
